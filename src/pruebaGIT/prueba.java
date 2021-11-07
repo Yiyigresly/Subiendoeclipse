c0d68e288363d741382532e89c6c9c92db445b20
Yiyigresly <keorza@gmail.com>
2021-11-07 11:55:03 +0100

    imprime for

--- src/pruebaGIT/prueba.java
+++ src/pruebaGIT/prueba.java
@@ -4,10 +4,13 @@ public class prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		
-		System.out.println("bienvenidos");
-		System.out.println("campeona");
+     int i;
+	
+		System.out.println("bienvenidos");//primer commit
+		System.out.println("campeona");//segundo comnmit
+		for (i=0 ;i<=10;i++ ) { //tercer commit
+			System.out.println(i);
+		}
 	}
 
 }
